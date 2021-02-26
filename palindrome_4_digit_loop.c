fa96a1f9bf385684fd6dfdd975d979738a3f5062
Kiran Chaudhary <chaudharyk456@gmail.com>
2021-02-26 16:41:57 +0545

    made reverse using loop easier to understand

--- palindrome_4_digit_loop.c
+++ palindrome_4_digit_loop.c
@@ -3,15 +3,13 @@
 void main(){
     int num, digit, copy;
     int rev = 0;
-    int multiplier = 1000;
     printf("\nEnter 4 digited number: ");
     scanf("%d", &num);
     copy = num;             // making copy of original number
     while(num > 0){
         digit = num % 10;
         num = num / 10;
-        rev = rev + digit * multiplier;     // reversing the number
-        multiplier = multiplier / 10;
+        rev = rev * 10 + digit;     // reversing the number
     }
     if(copy == rev){
         printf("%d is a palindrome number.", copy);
--- reverse_4_digit_loop.c
+++ reverse_4_digit_loop.c
@@ -5,15 +5,13 @@
 void main(){
     int num, digit, copy;
     int rev = 0;
-    int multiplier = 1000;
     printf("Enter a 4 digited number: ");
     scanf("%d", &num);
     copy = num;             //making copy of original number
     while(num > 0){
         digit = num % 10;
         num = num / 10;
-        rev = rev + digit * multiplier;
-        multiplier = multiplier / 10;
+        rev = rev * 10 + digit;
     }
     printf("\nOriginal number = %d", copy);
     printf("\nReverse number = %d", rev);
